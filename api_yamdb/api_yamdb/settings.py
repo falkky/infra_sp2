884de9fb9c34741951e96ba7f04693a450872d3d
Андрей Степанов <andrew.falkky@gmail.com>
2022-09-04 21:45:16 +0300

    api_yamdb in docker-compose

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -114,7 +114,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Переопределяемая модель User
 
 AUTH_USER_MODEL = 'reviews.User'
